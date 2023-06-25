2286833772ca8c3742cbb9f56b7f2a340ece6f31
Mel-TB <melinda.trami@live.fr>
2023-06-25 17:26:20 +0200

    optimizing with lazy, suspense, and codesplitting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentification/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
 import SignUpForm from './component/sign-up-form/sign-up-form.component';
-import ConfirmPage from './component/confirm/confirm.component';
+import Spinner from './component/spinner/spinner.component';
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() =>
+	import('./routes/authentification/authentication.component')
+);
+const Navigation = lazy(() =>
+	import('./routes/navigation/navigation.component')
+);
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const ConfirmPage = lazy(() => import('./component/confirm/confirm.component'));
 
 const App = () => {
 	const dispatch = useDispatch();
@@ -20,19 +26,21 @@ const App = () => {
 	}, [dispatch]);
 
 	return (
-		<Routes>
-			<Route path="/" element={<Navigation />}>
-				<Route index element={<Home />} />
-				<Route path="shop/*" element={<Shop />} />
+		<Suspense fallback={<Spinner />}>
+			<Routes>
+				<Route path="/" element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path="shop/*" element={<Shop />} />
 
-				<Route path="auth/*" element={<Authentication />} />
-				<Route path="/SignUpForm" element={<SignUpForm />} />
+					<Route path="auth/*" element={<Authentication />} />
+					<Route path="/SignUpForm" element={<SignUpForm />} />
 
-				<Route path="checkout" element={<Checkout />} />
+					<Route path="checkout" element={<Checkout />} />
 
-				<Route path="checkout/*" element={<ConfirmPage />} />
-			</Route>
-		</Routes>
+					<Route path="checkout/*" element={<ConfirmPage />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 };
 
